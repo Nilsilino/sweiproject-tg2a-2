9169a86cfc3ea06a0580dadc9deff0265dc58c35
Andreas Stiglmeier <stiglmeier.andreas@gmail.com>
2017-10-20 14:29:18 +0200

    java commited

--- Praktikum02/src/GeldBetrag.java
+++ Praktikum02/src/GeldBetrag.java
@@ -1,3 +1,12 @@
 public class GeldBetrag {
 
+	private int value;
+	
+	public GeldBetrag(int value) {
+		this.value = value;
+	}
+	
+	public int getBetrag() {
+		return value;
+	}
 }
--- Praktikum02/src/Konto.java
+++ Praktikum02/src/Konto.java
@@ -3,14 +3,20 @@ public class Konto {
 	
 	private String bezeichnung;
 	private Kunde zeichnungsberechtigter;
+	private GeldBetrag betrag;
 	
-	public Konto(Kunde zeichnungsberechtigter, String bezeichnung) {
+	public Konto(Kunde zeichnungsberechtigter, String bezeichnung, GeldBetrag betrag) {
 		this.zeichnungsberechtigter = zeichnungsberechtigter;
 		this.bezeichnung = bezeichnung;
+		this.betrag = betrag;
 	}
 	
 	public GeldBetrag saldo() {
-		
+		return betrag;
+	}
+	
+	public void einzahlen(GeldBetrag eingezahlterBetrag) {
+		betrag = new GeldBetrag(betrag.getBetrag() + eingezahlterBetrag.getBetrag());
 	}
 
 }
