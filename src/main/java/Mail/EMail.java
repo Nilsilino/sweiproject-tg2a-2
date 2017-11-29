4c6ed0453d4153c2ff81578a4f7a46a5eb1fd19d
Nilsilino <nils.wimmer@hotmail.com>
2017-11-29 22:51:06 +0100

    EMail

--- src/main/java/Mail/EMail.java
+++ src/main/java/Mail/EMail.java
@@ -1,4 +1,3 @@
-/*
 
 package Mail;
 
@@ -13,33 +12,42 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-*/
+
 
 /**
  * @author hm-nwimmer
  *
  */
-/*
+
 public class EMail {
 
 	private static final String SUBJECT = "Your personal link to Create/Change ur activity";
 	private static final String TEXT = "This is an auto-generated E-Mail. Please Click on the following link to create/edit an activity: localhost:8080/greetings";
 	private static final String FROM = "ActivityMeter2a2@gmail.com"; 
 	private static final String host = "smtp.gmail.com";
-	private static final String TO = "[I@7ea987ac";
-	private final String to;
-	private byte[] array;
+	private String to;
 	Properties properties = System.getProperties();
 	Session session;
 	MimeMessage message;
 	Transport transport;
 
 	public EMail(String to) {
-		// TODO Auto-generated constructor stub
-		this.to = to;
+			checkEmail(to);
+			sendEMail();
 	}
 
-
+	public void checkEmail(String to) {
+		if (to.endsWith("@hm.edu") || to.endsWith("@cpp.edu")) {
+			this.to = to;
+		} else {
+			try {
+				throw new Exception("No valid E-Mail Adress");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	String sendEMail () {
 
@@ -52,8 +60,7 @@ public class EMail {
             properties.put("mail.smtp.port", "465");
             properties.put("mail.smtp.ssl.enable", "true");
             session = Session.getInstance(properties, new javax.mail.Authenticator(){
-
-                    @Override
+            		@Override
                     protected PasswordAuthentication getPasswordAuthentication() {
                         System.out.println("Authenticating");
                         return new PasswordAuthentication(System.getenv("EMAIL_ADDRESS"), System.getenv("EMAIL_PASSWORD"));
@@ -77,4 +84,4 @@ public class EMail {
 
  }
 
-*/
+
