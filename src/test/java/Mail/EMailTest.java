5fbe82bbc56ef388e8d56261d4cd1df4639ad23f
nilsilino <nils.wimmer@hotmail.com>
2017-11-30 17:35:42 +0100

    Hi

---
+++ src/test/java/Mail/EMailTest.java
@@ -0,0 +1,29 @@
+package Mail;
+
+import static org.junit.Assert.*;
+
+import javax.mail.MessagingException;
+import javax.mail.SendFailedException;
+
+import org.junit.Test;
+
+public class EMailTest {
+
+	@Test
+	public void testSendEmail() {
+		EMail mail = new EMail("ActivityMeter2a2@gmail.com");
+		assertEquals(mail.sendEMail(), "Sent Message succesfully");
+	}
+	
+	@Test 
+	public void testCheckEmail() {
+		EMail mail = new EMail("ActivityMeter2a2@gmail.com");
+		assertFalse(mail.checkEmail("ActivityMeter2a2@gmail.com"));
+	}
+	
+	@Test(expected = SendFailedException.class) 
+	public void testMessagingException() {
+		EMail mail = new EMail("ActivityMeter2a2");
+	}
+	
+}
