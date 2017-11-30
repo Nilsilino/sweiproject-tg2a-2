2099029a0154fffe79b315c8a2d5d64f6ba5a0b7
Akavith <32568502+Akavith@users.noreply.github.com>
2017-11-30 21:22:43 +0100

    .

--- src/main/java/Mail/main.java
+++ src/main/java/Mail/main.java
@@ -5,11 +5,5 @@ package Mail;
 public class main {
 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		EMail mail = new EMail("");
-		// System.out.println(mail.sendEMail());
-		
-	}
 
 }
